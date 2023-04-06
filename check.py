c519baacf954b8ff7f17456502ec96f0d86c153d
GertyPieters <119414044+GertyPieters@users.noreply.github.com>
2023-04-06 12:36:25 +0200

    Update check.py

--- check.py
+++ check.py
@@ -1,3 +1,13 @@
-def sum(a, b):
-  return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Als t werkt dan werkt t!'
+
+@app.route('/real')
+def real():
+    return 'En dan echt!'
+
   
